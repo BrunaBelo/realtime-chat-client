2eff781a0c7d91a06e572f3633527e2a5b10b978
Higor Chaves <hhigorsilvachaves@gmail.com>
2023-02-26 16:21:16 -0300

    feat: add login page styles

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../../icons/logo';
-import { Container } from './style';
+import { Container, InputButton, InputForm } from './style';
+
+type UserInfo = {
+  username: string,
+  name?: string
+}
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({username: '', name: ''} as UserInfo);
+
   return (
-    <Container className="Login">
-      <h1>Welcome to carrier pigeon</h1>
-      <Logo color='#9F3FFF' width={100}/>
+    <Container>
+      <div className='content'>
+        <div className='title-div'>
+          <Logo color='#9F3FFF' width={100}/>
+          <h1>Welcome to <b>Carrier Pigeon</b></h1>
+        </div>
+
+        <form className='login-form' onSubmit={() => {}}>
+          <h2>Login</h2>
+          <InputForm 
+            placeholder='username'
+            value={userInfo.username}
+            onChange={(e) => setUserInfo({...userInfo, username: e.target.value})}
+          />
+          <InputButton>Enter</InputButton>
+        </form>
+      </div>
     </Container>
   )
 }
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -6,8 +6,58 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  color: #9F3FFF;
 
   h1 {
-    font-weight: bolder;
+    font-weight: normal;
+  }
+
+  .title-div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .login-form {
+    display: flex;
+    flex-direction: column;
+    width: auto;
+
+    h2 {
+      font-weight: normal;
+    }
+  }
+`
+
+export const InputForm = styled.input`
+  background-color: transparent;
+  margin: 0;
+  border: 0;
+  border-bottom: solid 2px rgba(159, 63, 255, .5);
+  margin-bottom: 5px;
+  padding: 10px 20px 10px 0px;
+  font-size: 18px;
+  color: white;
+  transition: .3s;
+
+  :focus {
+    outline: none;
+    border-bottom: solid 3px #9F3FFF;
+  }
+`
+
+export const InputButton = styled.button`
+  outline: none;
+  border: none;
+  margin-top: 5px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  background-color: #9F3FFF;
+  color: white;
+  font-weight: bold;
+  font-size: 16px;
+
+  :hover {
+    cursor: pointer;
   }
 `
